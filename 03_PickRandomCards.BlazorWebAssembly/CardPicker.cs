ef3c5a14da4e93aac4ed11d0ab8acd2e9b2cac5f
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-10-08 19:52:39 +0200

    Chapter 3 - pick random cards

---
+++ 03_PickRandomCards.BlazorWebAssembly/CardPicker.cs
@@ -0,0 +1,42 @@
+﻿namespace _03_PickRandomCards.BlazorWebAssembly;
+
+public class CardPicker
+{
+	private static readonly Random _random = new();
+
+	public static string[] PickSomeCards(int numberOfCards)
+	{
+		string[] cards = new string[numberOfCards];
+		for (int i = 0; i < numberOfCards; i++)
+		{
+			cards[i] = RandomValue() + " " + RandomSuit();
+		}
+		return cards;
+	}
+
+	private static string RandomValue()
+	{
+		int value = _random.Next(1, 14);
+		return value switch
+		{
+			1 => "As",
+			11 => "Walet",
+			12 => "Dama",
+			13 => "Król",
+			_ => value.ToString()
+		};
+	}
+
+	private static string RandomSuit()
+	{
+		int suit = _random.Next(1, 5);
+		return suit switch
+		{
+			1 => "pik",
+			2 => "kier",
+			3 => "trefl",
+			4 => "karo",
+			_ => throw new InvalidOperationException("Error! Bad suit!")
+		};
+	}
+}
